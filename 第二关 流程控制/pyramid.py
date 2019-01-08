4d66b2058494a6e8695b529dc08209d91f21eb81
hc_data <1197063125@qq.com>
2019-01-09 00:11:41 +0800

    修改了注释

--- 第二关 流程控制/pyramid.py
+++ 第二关 流程控制/pyramid.py
@@ -13,5 +13,5 @@ for current_level in range(1, max_level+1):
     for i in range(max_level-current_level):
         print(' ', end='')  # 在一行中连续打印多个空格
     for j in range(2*current_level-1):
-        print('*', end='')  # 在一行中连续打印多个空格
+        print('*', end='')  # 在一行中连续打印多个星号
     print()
